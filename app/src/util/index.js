22b40aec0914587d475cd1f935b2192a8b8d4a7b
tigerza117 <noppawanza1171@gmail.com>
2022-10-04 19:09:33 +0700

    fix: image prefix server select.

--- app/src/util/index.js
+++ app/src/util/index.js
@@ -2,9 +2,10 @@ const proxyURL = "http://127.0.0.1:8844/proxy"
 
 export function unescapeHTML(escapedHTML) {
     let t = escapedHTML.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&')
+    let regexSite = /image(\d+)/gm
     setTimeout(() => {
-        eval(t.replaceAll("style", `style`).replaceAll("image5", "127").replaceAll("src", " srxc").replaceAll("https", "http").replaceAll("niceoppai", "0.0").replaceAll("net", "1:8844/proxy").replaceAll("jpg", "jpg?prefix=image5"));
-    }, 500)
+        eval(t.replaceAll("https", "http").replaceAll(regexSite, "127").replaceAll("niceoppai", "0.0").replaceAll("net", "1:8844/proxy").replaceAll("jpg", "jpg?prefix="+t.match(regexSite)[0]));
+    }, 100)
     return t;
 }
 export function replaceProxy(text) {
